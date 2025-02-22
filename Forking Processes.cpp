57d3d1d481f9e456cb51be4ef6336b1907c4dd77
Ebu Karaoglu <112530588+N0zkill@users.noreply.github.com>
2025-02-22 04:09:36 -0500

    Update Forking Processes.cpp

--- Forking Processes.cpp
+++ Forking Processes.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 #define SIZE 20
 
-// Function to find the minimum in an array range
+
 int findMin(int arr[], int start, int end) {
     int minVal = INT_MAX;
     for (int i = start; i < end; i++) {
@@ -22,52 +22,52 @@ int findMin(int arr[], int start, int end) {
 
 int main() {
     int arr[SIZE];
-    int pipefd[2];  // Pipe for communication
+    int pipefd[2];  
     pid_t pid;
 
-    srand(time(0));  // Seed random number generator
+    srand(time(0));  
 
-    // Generate 20 random numbers
+    
     cout << "Generated Array: ";
     for (int i = 0; i < SIZE; i++) {
-        arr[i] = rand() % 100;  // Numbers between 0 and 99
+        arr[i] = rand() % 100;  
         cout << arr[i] << " ";
     }
     cout << endl;
 
-    // Create pipe
+    
     if (pipe(pipefd) == -1) {
         cerr << "Pipe creation failed!" << endl;
         return 1;
     }
 
-    // Forking a new process
+    
     pid = fork();
 
     if (pid < 0) {
         cerr << "Fork failed!" << endl;
         return 1;
     } 
-    else if (pid == 0) { // Child Process
-        close(pipefd[0]);  // Close unused read end
+    else if (pid == 0) { 
+        close(pipefd[0]);  
         int minChild = findMin(arr, SIZE / 2, SIZE);
         cout << "Child Process (PID: " << getpid() << ") found min: " << minChild << endl;
         write(pipefd[1], &minChild, sizeof(minChild));
-        close(pipefd[1]);  // Close write end
+        close(pipefd[1]);  
     } 
-    else { // Parent Process
-        close(pipefd[1]);  // Close unused write end
+    else { 
+        close(pipefd[1]);  
         int minParent = findMin(arr, 0, SIZE / 2);
         int minChild;
         read(pipefd[0], &minChild, sizeof(minChild));
-        close(pipefd[0]);  // Close read end
+        close(pipefd[0]);  
 
         cout << "Parent Process (PID: " << getpid() << ") found min: " << minParent << endl;
 
         int minOverall = (minParent < minChild) ? minParent : minChild;
         cout << "Overall Minimum: " << minOverall << endl;
 
-        wait(NULL);  // Wait for child to finish
+        wait(NULL);  
     }
 
     return 0;
